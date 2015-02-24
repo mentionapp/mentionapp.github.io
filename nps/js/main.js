08633dd23453bf323fb11e343142a025d0508626
Arnaud Le Blanc <arnaud.lb@gmail.com>
2015-02-24 19:15:22 +0100

    nps

---
+++ nps/js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+	$('.men-form-btn').click(function(e) {
+    var sEmail = $('.men-form-input').val();
+    if ($.trim(sEmail).length === 0) {
+      alert('Please enter valid email address');
+      e.preventDefault();
+    }
+    if (validateEmail(sEmail)) {
+    	e.preventDefault();
+    	$('.men-form').addClass('hidden');
+    	$('.men-form-success').removeClass('hidden');
+    	$('.men-form-success').addClass('visible');
+    }
+    else {
+      alert('Invalid Email Address');
+      e.preventDefault();
+    }
+	});
+  function validateEmail(sEmail) {
+		var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+		if (filter.test(sEmail)) {
+	    return true;
+	  }
+	  else {
+	    return false;
+	  }
+	}​
+	$('.men-form-input').focusout(function() {
+		var sEmail = $('.men-form-input').val();
+		console.log('valeur email :' + sEmail);
+		window.augurAPI = "http://api.augur.io/v2/user?key=ikxxvks77804a1n8a37dn0pt088q00qf&email="+sEmail;
+		$.getJSON(augurAPI).done(function(data){
+			console.log("success: we have to parse data");
+			console.log(data);
+			analytics.identify(''+sEmail+'', {
+				email: sEmail,
+			});
+			analytics.identify(null, {
+			  name: data.PRIVATE.name,
+			  gender: data.DEMOGRAPHICS.gender,
+			  city: data.GEOGRAPHICS.location_city,
+			  country: data.GEOGRAPHICS.location_country,
+			  twitter: data.PROFILES.twitter_handle,
+			  bioTwitter: data.PRIVATE.bio[0].value,
+			  linkedin: data.PROFILES.linkedin_handle
+			});
+		}).fail(function(){
+			console.log('failed to get json from augur API');
+		});
+		analytics.track('Registered for ebook', {
+		  ebookName : 'NPS'
+		});
+	});
+});
