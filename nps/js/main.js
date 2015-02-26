db96fadb5d55fe00ca0fa9ab397443279426c22e
Gilles Bertaux <gillesbertauxdorgeville@gmail.com>
2015-02-26 15:09:54 +0100

    up js

--- nps/js/main.js
+++ nps/js/main.js
@@ -30,24 +30,31 @@ $(document).ready(function() {
 		var anoID = analytics.user().anonymousId();
 		window.augurAPI = "http://api.augur.io/v2/user?key=ikxxvks77804a1n8a37dn0pt088q00qf&email="+sEmail;
 		$.getJSON(augurAPI).done(function(data){
-			console.log("success: we have to parse data");
+			console.log("got data");
 			console.log(data);
+			console.log('begin identify 1');
 			analytics.identify(''+ anoID +'', {
 				email: sEmail
 			});
-			analytics.identify(''+ sEmail +'', {
+			console.log('begin identify 2');
+			properties = {
 			  name: data.PRIVATE.name,
 			  gender: data.DEMOGRAPHICS.gender,
 			  city: data.GEOGRAPHICS.location_city,
 			  country: data.GEOGRAPHICS.location_country,
 			  twitter: data.PROFILES.twitter_handle,
-			  bioTwitter: data.PRIVATE.bio[0].value,
+			  bioTwitter: null,
 			  linkedin: data.PROFILES.linkedin_handle
-			});
+			}
+			if (data.PRIVATE.bio !== undefined && data.PRIVATE.bio.length > 0 ) {
+				properties.bioTwitter = data.PRIVATE.bio[0].value
+			};
+			analytics.identify(''+ sEmail +'', properties);
+			console.log('begin track');
 			analytics.track('Registered for ebook', { 
 				ebook: 'NPS'
 			});	
-			console.log('mxcnote: the end');
+			console.log('end');
 		}).fail(function(){
 			console.log('failed to get json from augur API');
 		});
