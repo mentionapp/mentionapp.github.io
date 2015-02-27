40d90f9de0aaa55c038f68a5b33c8b8ed4cd5b07
Gilles Bertaux <gillesbertauxdorgeville@gmail.com>
2015-02-27 17:08:58 +0100

    up js

--- nps/js/main.js
+++ nps/js/main.js
@@ -38,6 +38,7 @@ $(document).ready(function() {
 			});
 			console.log('begin identify 2');
 			properties = {
+				mail: sEmail,
 			  name: data.PRIVATE.name,
 			  gender: data.DEMOGRAPHICS.gender,
 			  city: data.GEOGRAPHICS.location_city,
