14c548ae2dc488b6141096080afae0b9287e8c1b
Gilles Bertaux <gillesbertauxdorgeville@gmail.com>
2015-05-21 18:15:14 +0200

    academy

---
+++ academy/js/min/main-min.js
@@ -0,0 +1,1 @@
+$(window).scroll(function(){var a=$(".ac__ctas--inplace").offset().top;console.log(a);var c=$(window);c.scrollTop()>=a&&($(".ac__ctas--inplace").addClass("ac__ctas--fixed"),$(".ac__ctas--inplace").removeClass("ac__ctas--inplace"))});
