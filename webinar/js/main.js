3a6a695ddc337628b2b35760dbfd263228a0df38
Gilles Bertaux <Gilles@MacBook-Air-de-Gilles.local>
2015-07-30 09:36:26 +0200

    segment + var date

--- webinar/js/main.js
+++ webinar/js/main.js
@@ -1,3 +1,19 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    	sURLVariables = sPageURL.split('&'),
+    	sParameterName,
+    	i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
+};
+
+var date_webinar = getUrlParameter('date');
+
 $( ".men__btn-main--wh" ).click(function(event) {
 	event.preventDefault();
 
@@ -16,7 +32,8 @@ $( ".men__btn-main--wh" ).click(function(event) {
 	console.log("identified");
 	
 	analytics.track('Registered for webinar', {
-  	webinar_name: 'Test'
+  	webinar_name: 'Test',
+  	webinar_date: date, 
 	});
 	console.log("tracked");
 
